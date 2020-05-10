adc8c3171fd7868a81dfc41f946abea8c7f5bdbd
Joshuabrotman <joshuabrotman@Gmail.com>
2020-05-10 17:48:15 -0500

    added API controller

---
+++ ActivitiesAPI/ActivitiesAPI/Controllers/ActivitiesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivitiesAPI.Controllers
+{
+
+    [Route("api/activities")]
+    [ApiController]
+    public class ActivitiesController : Controller
+    {
+        
+        // GET: Activities
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        // GET: Activities/Details/5
+        public string Details()
+        {
+            return "Hello";
+        }
+
+        // GET: Activities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Activities/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Activities/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Activities/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Activities/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Activities/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
