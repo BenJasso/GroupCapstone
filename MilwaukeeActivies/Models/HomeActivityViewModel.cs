370798e874777c8836c930e8ac1c5f48e4a27a44
Joshuabrotman <joshuabrotman@Gmail.com>
2020-05-13 16:58:44 -0500

    added the ability to filter by date, season, price, and indoor

---
+++ MilwaukeeActivies/Models/HomeActivityViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilwaukeeActivies.Models
+{
+    public class HomeActivityViewModel
+    {
+
+       public IEnumerable<Activities> Activities { get; set; }
+
+       public double MinBudget { get; set; }
+       public double MaxBudget { get; set; }
+
+        public DateTime dateStart { get; set; }
+        public DateTime dateEnd { get; set; }
+
+        public string season { get; set; }
+        public bool inside { get; set; }
+    }
+}
